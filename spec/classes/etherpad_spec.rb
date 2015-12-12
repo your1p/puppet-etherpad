2e7fd4676afacd7fc77a5a15832234ad1527655e
Igor Galić <i.galic@brainsware.org>
2015-12-12 22:20:48 +0100

    test(init): introduce tests for base-class
    two basic spec tests:
    
    * include etherpad
    * class { etherpad: all, params, set }
    
    These ensure that naked include works, and that all parameters are
    spelled correctly, and that their validation works.
    
    n.b.: Currently the tests are failing because our module is incomplete

---
+++ spec/classes/etherpad_spec.rb
@@ -0,0 +1,74 @@
+require 'spec_helper'
+
+describe 'etherpad' do
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context "etherpad class without any parameters" do
+          it { is_expected.to compile.with_all_deps }
+
+          it { is_expected.to contain_vcsrepo('/opt/etherpad') }
+          it { is_expected.to contain_file('/opt/etherpad/settings.json') }
+          it { is_expected.to contain_service('etherpad') }
+        end
+      end
+    end
+  end
+
+  context 'supported operating systems' do
+    on_supported_os.each do |os, facts|
+      context "on #{os}" do
+        let(:facts) do
+          facts
+        end
+
+        context "etherpad class with all parameters set" do
+          let(:params) do
+            {
+              :ensure         => 'present',
+              :service_name   => 'etherpad',
+              :service_ensure => 'running',
+              :manage_user    => true,
+              :manage_abiword => true,
+              :abiword_path   => '/usr/bin/abiword',
+              :user           => 'etherpad',
+              :group          => 'etherpad',
+              :root_dir       => '/opt/etherpad',
+              :source         => 'https://github.com/ether/etherpad-lite.git',
+
+              # Db
+              :database_type     => 'dirty',
+              :database_host     => 'localhost',
+              :database_user     => 'etherpad',
+              :database_name     => 'etherpad',
+              :database_password => 'etherpad',
+
+              # Network
+              :ip          => '*',
+              :port        => '9001',
+              :trust_proxy => :undef,
+
+              # Performance
+              :max_age => 21600,
+              :minify  => true,
+
+              # Config
+              :edit_only              => false,
+              :require_authentication => false,
+              :require_authorization  => false,
+              :comment                => :undef,
+              :default_pad_text       => 'Welcome to etherpad!',
+              :session_key            => 'some string',
+            }
+          end
+
+          it { is_expected.to compile.with_all_deps }
+        end
+      end
+    end
+  end
+end
