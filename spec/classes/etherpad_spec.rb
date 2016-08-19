51821f2dc2199eff13d804def7e44831f5c957e4
Tim Meusel <tim@bastelfreak.de>
2016-08-19 13:50:53 +0200

    rubocop: fix Style/HashSyntax

--- spec/classes/etherpad_spec.rb
+++ spec/classes/etherpad_spec.rb
@@ -29,43 +29,43 @@ describe 'etherpad' do
         context "etherpad class with all parameters set" do
           let(:params) do
             {
-              :ensure           => 'present',
-              :service_name     => 'etherpad',
-              :service_ensure   => 'running',
-              :service_provider => facts[:service_provider],
-              :manage_user      => true,
-              :manage_abiword   => false,
-              :abiword_path     => '/usr/bin/abiword',
-              :manage_tidy      => false,
-              :tidy_path        => '/usr/bin/tidy',
-              :user             => 'etherpad',
-              :group            => 'etherpad',
-              :root_dir         => '/opt/etherpad',
-              :source           => 'https://github.com/ether/etherpad-lite.git',
+              ensure: 'present',
+              service_name: 'etherpad',
+              service_ensure: 'running',
+              service_provider: facts[:service_provider],
+              manage_user: true,
+              manage_abiword: false,
+              abiword_path: '/usr/bin/abiword',
+              manage_tidy: false,
+              tidy_path: '/usr/bin/tidy',
+              user: 'etherpad',
+              group: 'etherpad',
+              root_dir: '/opt/etherpad',
+              source: 'https://github.com/ether/etherpad-lite.git',
 
               # Db
-              :database_type     => 'dirty',
-              :database_host     => 'localhost',
-              :database_user     => 'etherpad',
-              :database_name     => 'etherpad',
-              :database_password => 'etherpad',
+              database_type: 'dirty',
+              database_host: 'localhost',
+              database_user: 'etherpad',
+              database_name: 'etherpad',
+              database_password: 'etherpad',
 
               # Network
-              :ip          => '*',
-              :port        => 9001,
-              :trust_proxy => false,
+              ip: '*',
+              port: 9001,
+              trust_proxy: false,
 
               # Performance
-              :max_age => 21600,
-              :minify  => true,
+              max_age: 21600,
+              minify: true,
 
               # Config
-              :require_session        => false,
-              :edit_only              => false,
-              :require_authentication => false,
-              :require_authorization  => false,
-              :pad_title              => :undef,
-              :default_pad_text       => 'Welcome to etherpad!',
+              require_session: false,
+              edit_only: false,
+              require_authentication: false,
+              require_authorization: false,
+              pad_title: :undef,
+              default_pad_text: 'Welcome to etherpad!',
             }
           end
 
