0f5583992017d9f43af862822834bae463023861
Tim Meusel <tim@bastelfreak.de>
2017-06-11 14:33:46 +0200

    replace validate_* with datatypes

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,20 +6,20 @@
 #
 class etherpad (
   # General
-  String  $ensure         = 'present', # This should be a pattern, but right now that's too long
-  String  $service_name   = 'etherpad',
-  String  $service_ensure = 'running', # again, should be an enum…
+  String  $ensure                     = 'present', # This should be a pattern, but right now that's too long
+  String  $service_name               = 'etherpad',
+  String  $service_ensure             = 'running', # again, should be an enum…
   # what if the fact doesn't exist (yet) or is b0rked? use Optional.
   Optional[String]  $service_provider = $::service_provider,
-  Boolean $manage_user    = true,
-  Boolean $manage_abiword = false,
-  String  $abiword_path   = '/usr/bin/abiword',
-  Boolean $manage_tidy    = false,
-  String  $tidy_path      = '/usr/bin/tidy',
-  String  $user           = 'etherpad',
-  String  $group          = 'etherpad',
-  String  $root_dir       = '/opt/etherpad',
-  String  $source         = 'https://github.com/ether/etherpad-lite.git',
+  Boolean $manage_user                = true,
+  Boolean $manage_abiword             = false,
+  Stdlib::Absolutepath $abiword_path  = '/usr/bin/abiword',
+  Boolean $manage_tidy                = false,
+  Stdlib::Absolutepath  $tidy_path    = '/usr/bin/tidy',
+  String  $user                       = 'etherpad',
+  String  $group                      = 'etherpad',
+  Stdlib::Absolutepath $root_dir      = '/opt/etherpad',
+  String  $source                     = 'https://github.com/ether/etherpad-lite.git',
 
   # Db
   String  $database_type     = 'dirty',
@@ -57,10 +57,6 @@ class etherpad (
   Optional[Integer] $logconfig_file_backups       = undef,
   Optional[String]  $logconfig_file_category      = undef,
 ) {
-
-  validate_absolute_path($abiword_path)
-  validate_absolute_path($tidy_path)
-  validate_absolute_path($root_dir)
 
   unless $ensure =~
     Variant[Enum['present', 'latest', 'absent'], Pattern[/\A\d\.\d\.\d\Z/, /\A[a-fA-F0-9]{6,40}\Z/]] {
