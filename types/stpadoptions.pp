b739aaf042f6d7befec46a333665090355bc2d8f
your1p <youri.polydore@adullact.org>
2018-06-28 16:08:47 +0200

    Types modified for add Struct

--- types/stpadoptions.pp
+++ types/stpadoptions.pp
@@ -1,13 +1,15 @@
 # Matches Pad Options Keys And Values
-type Etherpad::Stpadoptions = Hash[Enum[
-    'noColors',        
-    'showControls',    
-    'showLineNumbers', 
-    'useMonospaceFont',
-    'userName', 
-    'userColor',       
-    'rtl',       
-    'alwaysShowChat',
-    'chatAndUsers',  
-    'lang',
-], Variant[Boolean, String]]
+type Etherpad::Stpadoptions = Struct[
+  {
+    Optional['noColors']         => Boolean,
+    Optional['showControls']     => Boolean,
+    Optional['showLineNumbers']  => Boolean,
+    Optional['useMonospaceFont'] => Boolean,
+    Optional['userName']         => Variant[Boolean, String],
+    Optional['userColor']        => Variant[Boolean, String],
+    Optional['rtl']              => Boolean,
+    Optional['alwaysShowChat']   => Boolean,
+    Optional['chatAndUsers']     => Boolean,
+    Optional['lang']             => String[1],
+  }
+]
